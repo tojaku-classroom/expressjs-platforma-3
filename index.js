f9f007303a61cc2a6945fc58017bea225bf1f422
Tomislav Jakupić <tojaku@gmail.com>
2025-11-26 11:16:50 +0100

    CR kontakata

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { engine } = require('express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
 const ajv = require('./lib/validation');
+const db = require('./lib/db');
 
 const app = express();
 
@@ -36,15 +37,65 @@ app.get('/contact', (request, response) => {
     response.render('contact');
 });
 
-app.post('/contact', (request, response) => {
+app.post('/contact', async (request, response) => {
     const data = request.body;
     const schema = require('./lib/schemas/contact');
     const validateData = ajv.compile(schema);
-    const isValid = validateData(data);
-    if (!isValid) {
+    const valid = validateData(data);
+    if (!valid) {
         console.log(validateData.errors);
+        response.render('contactData', { data: data, valid: valid, success: false });
+        return;
     }
-    response.render('contactData', { data: data, valid: isValid });
+
+    let success = false;
+    try {
+        data._id = Date.now().toString();
+        const result = await db.put(data);
+        success = true;
+    } catch (error) {
+        console.log(error);
+        success = false;
+    }
+
+    response.render('contactData', { data: data, valid: valid, success: success });
+});
+
+app.get('/contacts', async (request, response) => {
+    let items = null;
+    try {
+        items = await db.allDocs({ include_docs: true });
+        console.log(items);
+        items = items.rows.map(r => {
+            const doc = r.doc;
+            doc.date = new Date(Number(doc._id)).toLocaleDateString('hr');
+            return doc;
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    response.render('contacts', { items: items });
+});
+
+app.get('/contact/view/:id', async (request, response) => {
+    const data = request.params;
+    const schema = require('./lib/schemas/id');
+    const validateData = ajv.compile(schema);
+    const valid = validateData(data);
+    if(!valid) {
+        console.log(validateData.errors);
+        response.redirect('/error');
+    }
+
+    const messageId = data.id.toString();
+    try {
+        const doc = await db.get(messageId);
+        console.log(doc);
+        response.render('contactView', {item: doc});
+    } catch (error) {
+        console.log(error);
+        response.redirect('/error');
+    }
 });
 
 app.listen(port, () => {
