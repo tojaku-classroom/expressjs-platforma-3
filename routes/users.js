cb792d07eb74d8bac1a0ea3187d601968c80f2bb
Tomislav Jakupić <tojaku@gmail.com>
2026-01-13 11:10:59 +0100

    TailwindCSS

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log('Moja tajna: ' + process.env.MY_PASSWORD);
   res.send('respond with a resource');
 });
 
