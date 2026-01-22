383ace19ea786d053c0487719b0ea13913bd35fd
Tomislav Jakupić <tojaku@gmail.com>
2026-01-22 13:09:38 +0100

    Layout

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  console.log('Moja tajna: ' + process.env.MY_PASSWORD);
-  res.send('respond with a resource');
+router.get('/signin', function(req, res, next) {
+  res.render('signin');
 });
 
 module.exports = router;
