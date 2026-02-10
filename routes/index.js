59744df7e61a6bac69c24ec5a53e1b2b8ecf618b
Tomislav Jakupić <tojaku@gmail.com>
2026-02-10 11:12:13 +0100

    SQLite baza

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    try {
+        const testData = req.db.getAll('test');
+        res.render('index', { data: testData });
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
